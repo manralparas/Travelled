3536cc7fb80722b29aa5b0c2308167b7d049669e
manralparas <manralparas@gmail.com>
2021-01-06 13:03:23 +0530

    add package.json and remove reference.js

--- reference.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://paras:CFMFV0UOQPA1Y8Ic@cluster0.y8o7g.mongodb.net/camp?retryWrites=true&w=majority",{ useNewUrlParser: true ,useUnifiedTopology: true,});
-const Post = require("./models/campground");
-const User = require("./models/user");
-
-
